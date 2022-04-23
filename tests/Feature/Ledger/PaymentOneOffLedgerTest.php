40e870baae7443452f4ed1789a08c2c3eae94c47
Miriam Franke <miriam.franke77@gmail.com>
2022-04-23 12:44:52 +0200

    cleaned up tests

--- tests/Feature/Ledger/PaymentOneOffLedgerTest.php
+++ tests/Feature/Ledger/PaymentOneOffLedgerTest.php
@@ -68,18 +68,7 @@ class PaymentOneOffLedgerTest extends TestCase
         );
 
         $actual = Payment::first();
-
-        $this->assertEquals($expected->user_id, $actual->user_id);
-        $this->assertEquals($expected->account_type, $actual->account_type);
-        $this->assertEquals($expected->payment_type, $actual->payment_type);
-        $this->assertEquals($expected->shop_id, $actual->shop_id);
-        $this->assertEquals($expected->title, $actual->title);
-        $this->assertEquals($expected->amount, $actual->amount);
-        $this->assertEquals($expected->category_id, $actual->category_id);
-        $this->assertEquals($expected->description, $actual->description);
-        $this->assertEquals($expected->interval, $actual->interval);
-        $this->assertEquals($expected->starts_at, $actual->starts_at);
-        $this->assertEquals($expected->ends_at, $actual->ends_at);
+        $this->assertPaymentsEqual($expected, $actual);
     }
 
     /** @test */
@@ -118,18 +107,7 @@ class PaymentOneOffLedgerTest extends TestCase
         );
 
         $actual = Payment::first();
-
-        $this->assertEquals($expected->user_id, $actual->user_id);
-        $this->assertEquals($expected->account_type, $actual->account_type);
-        $this->assertEquals($expected->payment_type, $actual->payment_type);
-        $this->assertEquals($expected->shop_id, $actual->shop_id);
-        $this->assertEquals($expected->title, $actual->title);
-        $this->assertEquals($expected->amount, $actual->amount);
-        $this->assertEquals($expected->category_id, $actual->category_id);
-        $this->assertEquals($expected->description, $actual->description);
-        $this->assertEquals($expected->interval, $actual->interval);
-        $this->assertEquals($expected->starts_at, $actual->starts_at);
-        $this->assertEquals($expected->ends_at, $actual->ends_at);
+        $this->assertPaymentsEqual($expected, $actual);
     }
 
     /** @test */
